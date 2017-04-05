17f8f0909dbfd71ab232a8013ed731302839ff1f
Juwan Wheatley <datwheat@gmail.com>
2017-04-05 12:23:36 -0400

    change to helloworld to auto change name

--- index.ios.js
+++ index.ios.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-export default class substandard extends Component {
+export default class HelloWorld extends Component {
   render() {
     return <App />;
   }
 }
 
-AppRegistry.registerComponent('substandard', () => substandard);
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
